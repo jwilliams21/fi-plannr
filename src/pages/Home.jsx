027ee896c0c9164d3c16d4c8d6058165c4079983
Josh Williams <joshbenjaminwilliams@gmail.com>
2026-05-16 18:04:22 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import {  } from '../components';  //points at ./components/index.js by default
+
 
 export default function Home() {
 
   return (
     <>
-      
+      <h1>FI - PlannR</h1>
     </>
   )
 }
