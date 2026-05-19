8343c0132f0b1812df336545d211e229cdd1ad6b
Josh Williams <joshbenjaminwilliams@gmail.com>
2026-05-19 18:09:07 -0400

    Add initial code for FiInputs and include InputField

--- src/components/FiInputs.jsx
+++ src/components/FiInputs.jsx
@@ -1,11 +1,14 @@
-
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { fireFormSchema } from '../schemas/fireSchema';
+import { InputField } from '../../../components/ui/InputField';
 
 export default function FiInputs() {
 
   return (
-    <div>
-      <label></label>
-      <input type="text" placeholder="Enter text here" name="startingAmount" />
-    </div>
+    <form>
+      <InputField />
+    </form>
   )
 }
